25a364d9d556437dbf4f53a65cc8e1c72f940e24
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-11-16 16:38:06 +0530

    Array, basics.

---
+++ Piyush_JS/API/gitHub/script.js
@@ -0,0 +1,18 @@
+// const names = "hiteshchoudhary";
+// async function gitHubApi(names) {
+//   const promise = await fetch(`https://api.github.com/users/${names}`);
+//   const parsed_data = await promise.json();
+//   //   console.log(parsed_data);
+//   return "value";
+// }
+
+// // console.log(gitHubApi(names));
+// async function parse() {
+//   console.log(await gitHubApi(names));
+// }
+
+// parse();
+// const username = process.argv[2];
+// console.log("UserName: ", username);
+// const username2 = process.argv[3];
+// console.log("UserName2: ", username2);
---
+++ chai_code_Array/01_arrays.js
@@ -0,0 +1,15 @@
+const a = [0, 1, 2, 3, 4, 55];
+
+// a.push(91);
+// console.log("A: ", a);
+// a.pop();
+// console.log("A: ", a);
+// a.unshift(666);
+// console.log("A: ", a);
+// a.shift();
+// console.log("A: ", a);
+
+// console.log(a.slice(1, 3));
+// console.log("a: ", a);
+// console.log(a.splice(1, 3));
+// console.log("a: ", a);
